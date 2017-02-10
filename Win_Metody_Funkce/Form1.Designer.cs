7ddf1bb9f1ef5ed5034d0f7c2cd97e9fbe085f32
alesg <alesg@DESKTOP-HOUSE>
2017-02-10 22:37:13 +0100

    Add project files.

---
+++ Win_Metody_Funkce/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Win_Metody_Funkce
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_box1 = new System.Windows.Forms.TextBox();
+            this.txt_box2 = new System.Windows.Forms.TextBox();
+            this.txt_box3 = new System.Windows.Forms.TextBox();
+            this.txt_box4 = new System.Windows.Forms.TextBox();
+            this.txt_box5 = new System.Windows.Forms.TextBox();
+            this.txt_box6 = new System.Windows.Forms.TextBox();
+            this.btn_upravit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_box1
+            // 
+            this.txt_box1.Location = new System.Drawing.Point(27, 12);
+            this.txt_box1.Multiline = true;
+            this.txt_box1.Name = "txt_box1";
+            this.txt_box1.Size = new System.Drawing.Size(236, 225);
+            this.txt_box1.TabIndex = 0;
+            this.txt_box1.Text = "Ahoj Jak se mas a co delas Davide. Budes cestovvat s Lemkou nebo ne.";
+            this.txt_box1.TextChanged += new System.EventHandler(this.txt_box1_TextChanged);
+            // 
+            // txt_box2
+            // 
+            this.txt_box2.Location = new System.Drawing.Point(281, 12);
+            this.txt_box2.Multiline = true;
+            this.txt_box2.Name = "txt_box2";
+            this.txt_box2.Size = new System.Drawing.Size(236, 225);
+            this.txt_box2.TabIndex = 1;
+            this.txt_box2.Text = "Ahoj Jak se mas a co delas Davide. Budes cestovvat s Lemkou nebo ne.";
+            // 
+            // txt_box3
+            // 
+            this.txt_box3.Location = new System.Drawing.Point(536, 12);
+            this.txt_box3.Multiline = true;
+            this.txt_box3.Name = "txt_box3";
+            this.txt_box3.Size = new System.Drawing.Size(236, 225);
+            this.txt_box3.TabIndex = 2;
+            this.txt_box3.Text = "Ahoj Jak se mas a co delas Davide. Budes cestovvat s Lemkou nebo ne.";
+            // 
+            // txt_box4
+            // 
+            this.txt_box4.Location = new System.Drawing.Point(27, 255);
+            this.txt_box4.Multiline = true;
+            this.txt_box4.Name = "txt_box4";
+            this.txt_box4.Size = new System.Drawing.Size(236, 225);
+            this.txt_box4.TabIndex = 3;
+            this.txt_box4.Text = "Ahoj Jak se mas a co delas Davide. Budes cestovvat s Lemkou nebo ne.";
+            // 
+            // txt_box5
+            // 
+            this.txt_box5.Location = new System.Drawing.Point(281, 255);
+            this.txt_box5.Multiline = true;
+            this.txt_box5.Name = "txt_box5";
+            this.txt_box5.Size = new System.Drawing.Size(236, 225);
+            this.txt_box5.TabIndex = 4;
+            this.txt_box5.Text = "Ahoj Jak se mas a co delas Davide. Budes cestovvat s Lemkou nebo ne.";
+            // 
+            // txt_box6
+            // 
+            this.txt_box6.Location = new System.Drawing.Point(536, 255);
+            this.txt_box6.Multiline = true;
+            this.txt_box6.Name = "txt_box6";
+            this.txt_box6.Size = new System.Drawing.Size(236, 225);
+            this.txt_box6.TabIndex = 5;
+            this.txt_box6.Text = "Ahoj Jak se mas a co delas Davide. Budes cestovvat s Lemkou nebo ne.";
+            // 
+            // btn_upravit
+            // 
+            this.btn_upravit.Location = new System.Drawing.Point(902, 70);
+            this.btn_upravit.Name = "btn_upravit";
+            this.btn_upravit.Size = new System.Drawing.Size(75, 23);
+            this.btn_upravit.TabIndex = 6;
+            this.btn_upravit.Text = "Upravit";
+            this.btn_upravit.UseVisualStyleBackColor = true;
+            this.btn_upravit.Click += new System.EventHandler(this.btn_upravit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 635);
+            this.Controls.Add(this.btn_upravit);
+            this.Controls.Add(this.txt_box6);
+            this.Controls.Add(this.txt_box5);
+            this.Controls.Add(this.txt_box4);
+            this.Controls.Add(this.txt_box3);
+            this.Controls.Add(this.txt_box2);
+            this.Controls.Add(this.txt_box1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_box1;
+        private System.Windows.Forms.TextBox txt_box2;
+        private System.Windows.Forms.TextBox txt_box3;
+        private System.Windows.Forms.TextBox txt_box4;
+        private System.Windows.Forms.TextBox txt_box5;
+        private System.Windows.Forms.TextBox txt_box6;
+        private System.Windows.Forms.Button btn_upravit;
+    }
+}
+
---
+++ Win_Metody_Funkce/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win_Metody_Funkce
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_upravit_Click(object sender, EventArgs e)
+        {
+            // manualne napsano pro jeden textbox
+            string text1 = txt_box1.Text;
+            text1 = text1.Replace("Davide","Karle");
+            txt_box1.Text = text1;
+
+            // pouzita funkce
+            string slova = ReplaceText(txt_box2.Text);
+            txt_box2.Text = slova;
+
+            txt_box3.Text = ReplaceText(txt_box3.Text);
+            txt_box4.Text = ReplaceText(txt_box4.Text);
+            txt_box5.Text = ReplaceText(txt_box5.Text);
+            txt_box6.Text = ReplaceText(txt_box6.Text);
+
+        }
+
+
+        // vytvorena funkce
+        public string ReplaceText (string Text)
+        {
+            Text = Text.Replace("Davide", "Karle");
+            Text = Text + Environment.NewLine + Text.Length;
+            return Text;
+        }
+
+        private void txt_box1_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
